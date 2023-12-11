322f729b7dfe2fbd02f463d34b852c193578ce69
JaniHakalaYH <jani.hakala@yh.nackademin.se>
2023-12-11 12:29:36 +0100

    ternary operator added

--- src/index.js
+++ src/index.js
@@ -69,15 +69,23 @@ function Header() {
 }
 
 function Menu() {
+  //const pizzas = [];
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObject={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 ? (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObject={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : (
+        <p>We're still working on our menu. please come back later :)</p>
+      )}
     </main>
   );
 }
@@ -97,7 +105,7 @@ function Pizza(props) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 8;
+  const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
@@ -107,15 +115,18 @@ function Footer() {
   //comment to commit again
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
       )}
     </footer>
   );
-  //return React.createElement("footer", null, "We're currently open");
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
